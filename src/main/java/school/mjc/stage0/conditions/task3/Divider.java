8ec4e26e4f9c0725471bcfe757f37d3480e0e95f
Zhakshylyk Kasymbekov <jakkasymbekov@gmail.com>
2023-09-02 17:26:20 +0600

    Divider task commit after the third testing

--- src/main/java/school/mjc/stage0/conditions/task3/Divider.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Divider.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.task3;
 
 public class Divider {
     public void isDividableBy5And11(int number) {
-        if (number % 5 == 0 && number % 11 == 0) {
+        if (5 % number == 0 && 11 % number == 0) {
             System.out.println("Dividable");
         } else if (number == 0) {
             System.out.println("Cannot divide by zero");
