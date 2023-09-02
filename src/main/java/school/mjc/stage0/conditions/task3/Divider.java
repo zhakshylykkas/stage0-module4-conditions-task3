5185336951ef1f5b9ef2831c573885e789ea2438
Zhakshylyk Kasymbekov <jakkasymbekov@gmail.com>
2023-09-02 17:38:26 +0600

    Divider task commit after the fourth testing (turns out that the order of conditions matters)

--- src/main/java/school/mjc/stage0/conditions/task3/Divider.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Divider.java
@@ -2,10 +2,10 @@ package school.mjc.stage0.conditions.task3;
 
 public class Divider {
     public void isDividableBy5And11(int number) {
-        if (5 % number == 0 && 11 % number == 0) {
-            System.out.println("Dividable");
-        } else if (number == 0) {
+        if (number == 0) {
             System.out.println("Cannot divide by zero");
+        } else if (number % 5 == 0 && number % 11 == 0) {
+            System.out.println("Dividable");
         } else {
             System.out.println("Non-dividable");
         }
